a52cfcb632c5c77f23aafd1c92db5d492668a123
Milon Hossen <milonhosain382@gmail.com>
2025-12-31 00:15:48 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 # ===================== CONFIG =====================
 API_ID = 22134923
 API_HASH = "d3e9d2f01d3291e87ea65298317f86b8"
-BOT_TOKEN = "8046672368:AAGx5yzAUwF-8voOqUu1xwBFuCBGa5-iCPc"
+BOT_TOKEN = "8046672368:AAEN5l_oCsp4NwVnAsVKj2RanigmtIT2l6s"
 OWNER_ID = 7383046042
 # ==================================================
 
